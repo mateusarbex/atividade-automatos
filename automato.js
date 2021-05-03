8d1ea49538869ae2778d5c59f302c61fada77a3c
Mateus Arbex <51658233+mateusarbex@users.noreply.github.com>
2021-05-02 22:11:14 -0400

    final

--- automato.js
+++ automato.js
@@ -1,7 +1,19 @@
+/**
+ * Código para representação do autômato criado no JFLAP
+ * O Autômato criado segue o padrão AFN
+ * O modelo projeta que todos os estados do autômato partem do estado inicial
+ * Todos os estados não-iniciais também não são estados finais
+ * Os inputs de testes podem ser incluídos no arquivo senhas.txt
+ * As palavras do blacklist estão incluídas no arquivo blacklist.txt
+ */
+
 const fs = require("fs");
 
 const digitsEnum = { 0: "o", 1: "i", 3: "e", 4: "a", 5: "s", 7: "t" };
 
+//Função para adicionar a palavra de blacklist como estado ao autômato
+//A função recebe um string que dita a regra de transição para o autômato
+//A estrutura utiliza state como nome do estado, transition como regra de transição, isInitial para determinar se o estado é inicial e isFinal para determinar se o estado é final
 function setBlackListToNDFA(word) {
   const state = {
     state: "q" + (automato.length + ""),
@@ -12,7 +24,9 @@ function setBlackListToNDFA(word) {
   return automato.push(state);
 }
 
-//substituição de digitos para letras
+//Função para substituir os números referentes aos caracteres
+//Utiliza o regex /[01357]+/ para selecionar os números que se enquandram no enum digitsEnum
+//Para cada match do regex, utiliza-se a função replace do javascript para trocar o numéro pelo caractere do enum
 function formatWord(word) {
   const digitMatch = new RegExp(/[01357]+/g);
 
@@ -26,6 +40,7 @@ function formatWord(word) {
   return word;
 }
 
+//Função para dar início ao automático quando o código rodar
 function initAutomato() {
   const state = {
     state: "q" + (automato.length + ""),
@@ -37,6 +52,11 @@ function initAutomato() {
   automato.push(state);
 }
 
+//Função de validação do autômato utilizando a input como para verificar se o mesmo se encontra em uma regra de transição a partir do estado inicial
+//Utiliza-se a função find do javascript para encontrar qual o estado inicial do array automato
+//Após achar o estado inicial, se utiliza a mesma função para encontrar um dos estados que contenha a palavra em sua regra de transição
+//Se encontrado a regra de transição em algum estado, se transfere o estado atual para o estado que contenha a regra de transição
+//Após terminado, verifica se o estado atual é estado final para validação.
 function validate(word) {
   let currentState = automato.find((state) => state.inital);
   const formatted = formatWord(word);
@@ -56,21 +76,30 @@ const automato = [];
 
 initAutomato();
 
+//Função para leitura de palavras que da blacklist
 const rawdata = fs.readFileSync("./blacklist.txt", { encoding: "utf-8" });
 
 const blacklist = rawdata.split("\r\n");
 
 blacklist.forEach((value) => setBlackListToNDFA(value));
 
+//Função para leitura de inputs
 const senhasdata = fs.readFileSync("./senhas.txt", { encoding: "utf-8" });
 
 const senhas = senhasdata.split("\r\n");
 
+//Validação das senhas incluídas no arquivo senhas.txt
 senhas.forEach((senha) => {
   if (validate(senha.toLowerCase())) {
-    console.log("senha valida");
+    console.log(
+      "senha não contém palavras da blacklist, a senha em questão é",
+      senha
+    );
   } else {
-    console.log("senha inválida");
+    console.log(
+      "senha contém palavras da blacklist, a senha em questão é",
+      senha
+    );
   }
 });
 
